4f8894b4ad18b84158b0bcf5836d622f64707569
太皞 <wang2695691406@icloud.com>
2019-08-11 16:08:37 +0800

    update shell run-in-docker.sh

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -28,3 +28,7 @@ echo 'docker任务正在执行: run docker ....'
 sleep 2s
 docker run -p 8080:8080  --name dalaran -d dalaran:latest
 docker ps -a
+
+echo 'docker应用启动成功: 检查集成平台服务接口....'
+sleep 2s
+curl -X POST   http://localhost:8080/author/ask  -H 'content-type: application/json'  -d '{ "name":"visitor name"}'
